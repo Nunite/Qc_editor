d43424d7263342e3875515fe0649b02c1c21fe7d
Nunite <1318205957@qq.com>
2024-09-24 01:42:01 +0800

    修复了GUI尺寸问题，并添加了按钮颜色更改功能

--- Qc_edtior/Src/main.py
+++ Qc_edtior/Src/main.py
@@ -2,16 +2,18 @@ import tkinter as tk
 
 def myfunction1():
     print("Hello World")
+    #更改button0的颜色
+    button0.config(text='already clicked')
 
 def myfunction2():
     print("Goodbye World")
 
 window = tk.Tk()
 window.title("My First GUI")
-window.geometry("400x300")
+window.geometry("900x800")
 
 # 创建第一个按钮
-button0 = tk.Button(window, text='1: 根据标记内容生成', command=myfunction1)
+button0 = tk.Button(window, text='1: 根据标记内容生成', fg='red',command=myfunction1)
 button1 = tk.Button(window, text='2: 根据手动输入生成', command=myfunction2)
 button0.pack(pady=10) 
 # 创建第二个按钮
